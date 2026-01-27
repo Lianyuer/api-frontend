0833891b799a1ac0981415a337d9785280d15c71
lianyu <lian_cx@163.com>
2026-01-27 21:27:38 +0800

    fix: 修复获取接口详情不存在的问题

--- src/pages/interfaceInfo/index.tsx
+++ src/pages/interfaceInfo/index.tsx
@@ -1,9 +1,9 @@
-import {PageContainer} from '@ant-design/pro-components';
-import React, {useEffect, useState} from 'react';
-import {useMatch, useParams} from 'react-router';
-import {Badge, Card, Descriptions, message} from "antd";
-import {getInterfaceInfoVoByIdUsingGet} from "@/services/api-backend/interfaceInfoController";
+import { getInterfaceInfoVoByIdUsingGet } from "@/services/api-backend/interfaceInfoController";
+import { PageContainer } from "@ant-design/pro-components";
+import { Badge, Card, Descriptions, message } from "antd";
 import dayjs from "dayjs";
+import React, { useEffect, useState } from "react";
+import { useMatch, useParams } from "react-router";
 
 /**
  * 主页
@@ -15,7 +15,7 @@ const Index: React.FC = () => {
   const [data, setData] = useState<API.InterfaceInfo>();
   // 使用useMatch钩子将当前URL与指定的路径模式/interface_info/:id进行匹配，
   // 并将匹配结果赋值给match变量
-  const match = useMatch('/interfaceInfo/:id');
+  const match = useMatch("/interfaceInfo/:id");
   const params = useParams();
 
   const loadData = async (current = 1, pageSize = 5) => {
@@ -26,10 +26,10 @@ const Index: React.FC = () => {
     }
     setLoading(true);
     try {
-      const res = await getInterfaceInfoVoByIdUsingGet({id: Number(params.id)});
+      const res = await getInterfaceInfoVoByIdUsingGet({ id: params.id });
       setData(res.data);
     } catch (error: any) {
-      message.error('请求失败，' + error.message);
+      message.error("请求失败，" + error.message);
     }
     setLoading(false);
   };
@@ -45,21 +45,42 @@ const Index: React.FC = () => {
           <Descriptions title="接口详情" column={1}>
             <Descriptions.Item label="接口id">{data?.id}</Descriptions.Item>
             <Descriptions.Item label="接口名称">{data?.name}</Descriptions.Item>
-            <Descriptions.Item label="接口描述">{data?.description}</Descriptions.Item>
+            <Descriptions.Item label="接口描述">
+              {data?.description}
+            </Descriptions.Item>
             <Descriptions.Item label="接口地址">{data?.url}</Descriptions.Item>
-            <Descriptions.Item label="请求头">{data?.requestHeader}</Descriptions.Item>
-            <Descriptions.Item label="响应头">{data?.responseHeader}</Descriptions.Item>
-            <Descriptions.Item label="状态">{
-              data?.status ? <><Badge status="success"/>&nbsp;开启</> :
-                <><Badge status="error"/>&nbsp;关闭</>
-            }</Descriptions.Item>
-            <Descriptions.Item label="请求类型">{data?.method}</Descriptions.Item>
-            <Descriptions.Item
-              label="创建时间">{dayjs(data?.createTime).format('YYYY-MM-DD HH:mm:ss')}</Descriptions.Item>
-            <Descriptions.Item
-              label="更新时间">{dayjs(data?.updateTime).format('YYYY-MM-DD HH:mm:ss')}</Descriptions.Item>
+            <Descriptions.Item label="请求头">
+              {data?.requestHeader}
+            </Descriptions.Item>
+            <Descriptions.Item label="响应头">
+              {data?.responseHeader}
+            </Descriptions.Item>
+            <Descriptions.Item label="状态">
+              {data?.status ? (
+                <>
+                  <Badge status="success" />
+                  &nbsp;开启
+                </>
+              ) : (
+                <>
+                  <Badge status="error" />
+                  &nbsp;关闭
+                </>
+              )}
+            </Descriptions.Item>
+            <Descriptions.Item label="请求类型">
+              {data?.method}
+            </Descriptions.Item>
+            <Descriptions.Item label="创建时间">
+              {dayjs(data?.createTime).format("YYYY-MM-DD HH:mm:ss")}
+            </Descriptions.Item>
+            <Descriptions.Item label="更新时间">
+              {dayjs(data?.updateTime).format("YYYY-MM-DD HH:mm:ss")}
+            </Descriptions.Item>
           </Descriptions>
-        ) : <>接口不存在</>}
+        ) : (
+          <>接口不存在</>
+        )}
       </Card>
     </PageContainer>
   );
