7f3969741f430722a73f270ec907e5d8ac833611
lianyu <1319764245@qq.com>
2026-01-29 13:35:19 +0800

    feat: 新增全局响应拦截-未登录处理

--- src/requestErrorConfig.ts
+++ src/requestErrorConfig.ts
@@ -1,6 +1,7 @@
 ﻿import type { RequestOptions } from '@@/plugin-request/request';
 import type { RequestConfig } from '@umijs/max';
 import { message, notification } from 'antd';
+import { history } from 'umi';
 
 // 错误处理方案： 错误类型
 enum ErrorShowType {
@@ -10,6 +11,7 @@ enum ErrorShowType {
   NOTIFICATION = 3,
   REDIRECT = 9,
 }
+
 // 与后端约定的响应数据格式
 interface ResponseStructure {
   success: boolean;
@@ -100,9 +102,10 @@ export const errorConfig: RequestConfig = {
       // 拦截响应数据，进行个性化处理
       const { data } = response as unknown as ResponseStructure;
 
-      if (data?.success === false) {
-        message.error('请求失败！');
+      if (data.code === 40100 && window.location.pathname !== '/user/login') {
+        history.push('/user/login');
       }
+
       return response;
     },
   ],
